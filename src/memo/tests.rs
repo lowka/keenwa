258cd3c06dff58b2eb90c0e4eb7af6ef87f3d120
Max Zhuravkov <shhwwa@gmail.com>
2022-03-27 11:17:41 +0300

    memo: remove unsafe memo impl.

--- src/memo/tests.rs
+++ src/memo/tests.rs
@@ -1,3 +1,74 @@
+use crate::memo::{format_memo, GroupId, Memo, MemoBuilder, MemoExpr, MemoExprRef, MemoGroupRef};
+
+pub fn new_memo<E>() -> Memo<E, ()>
+where
+    E: MemoExpr,
+{
+    MemoBuilder::new(()).build()
+}
+
+pub fn insert_group<E, T>(memo: &mut Memo<E, T>, expr: E) -> (GroupId, MemoExprRef<E>)
+where
+    E: MemoExpr,
+{
+    let expr = memo.insert_group(expr);
+    let group_id = expr.state().memo_group_id();
+    let expr_ref = expr.state().memo_expr();
+
+    (group_id, expr_ref)
+}
+
+pub fn insert_group_member<E, T>(memo: &mut Memo<E, T>, group_id: GroupId, expr: E) -> (GroupId, MemoExprRef<E>)
+where
+    E: MemoExpr,
+{
+    let group = memo.get_group(&group_id);
+    let token = group.to_group_token();
+    let expr = memo.insert_group_member(token, expr);
+
+    let expr_ref = expr.state().memo_expr();
+
+    (group_id, expr_ref)
+}
+
+pub fn expect_group_size<E, T>(memo: &Memo<E, T>, group_id: &GroupId, size: usize)
+where
+    E: MemoExpr,
+{
+    let group = memo.get_group(group_id);
+    assert_eq!(group.mexprs().count(), size, "group#{}", group_id);
+}
+
+pub fn expect_group_exprs<E, T>(group: &MemoGroupRef<E, T>, expected: Vec<MemoExprRef<E>>)
+where
+    E: MemoExpr,
+{
+    let actual: Vec<MemoExprRef<E>> = group.mexprs().collect();
+    assert_eq!(actual, expected, "group#{} exprs", group.id());
+}
+
+pub fn expect_memo<E, T>(memo: &Memo<E, T>, expected: &str)
+where
+    E: MemoExpr,
+{
+    let lines: Vec<String> = expected.split('\n').map(String::from).collect();
+    let expected = lines.join("\n");
+
+    let buf = format_memo(&memo);
+    assert_eq!(buf.trim(), expected.trim());
+}
+
+pub fn expect_memo_with_props<E, T>(memo: &Memo<E, T>, expected: &str)
+where
+    E: MemoExpr,
+{
+    let lines: Vec<String> = expected.split('\n').map(String::from).collect();
+    let expected = lines.join("\n");
+
+    let buf = format_memo(&memo);
+    assert_eq!(buf.trim(), expected.trim());
+}
+
 #[cfg(test)]
 mod test {
     use crate::error::OptimizerError;
@@ -11,7 +82,7 @@ mod test {
         MemoExprState, MemoGroupCallback, NewChildExprs, Props, StringMemoFormatter,
     };
 
-    use super::super::testing::*;
+    use super::*;
 
     type RelNode = crate::memo::RelNode<TestOperator>;
     type ScalarNode = crate::memo::ScalarNode<TestOperator>;
